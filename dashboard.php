8a544bd8aae02782ff1bcad825787cc95396240e
FedeTardi <ftarditicp@gmail.com>
2024-05-13 18:03:41 +0200

    Creazione dashboard

---
+++ dashboard.php
@@ -0,0 +1,10 @@
+<?php include('partials/header.php') ?>
+<?php include('partials/navbarLogged.php') ?>
+
+<body>
+    <div class="m-3 col-lg-2 col-xl-1 align-items-center order-1">
+        <img src="images/User-Profile.png" class="img-fluid">
+    </div>
+</body>
+
+<?php include('partials/footer.php') ?>
