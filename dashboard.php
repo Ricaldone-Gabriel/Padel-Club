0d3af03395f575154cb4c8fce5b7633dbd20a068
FedeTardi <ftarditicp@gmail.com>
2024-05-13 18:09:32 +0200

    Dashboard nuova

--- dashboard.php
+++ dashboard.php
@@ -1,9 +1,11 @@
 <?php include('partials/header.php') ?>
 <?php include('partials/navbarLogged.php') ?>
 
+
+
 <body>
     <div class="m-3 col-lg-2 col-xl-1 align-items-center order-1">
-        <img src="images/User-Profile.png" class="img-fluid">
+        <img src="images/User-Icon.jpg" class="img-fluid">
     </div>
 </body>
 
