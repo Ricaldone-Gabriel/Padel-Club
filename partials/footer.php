f693e953e7f0c9504c05376bc842110d59e69d2f
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2024-05-13 18:08:09 +0200

    Footer nuovo

--- partials/footer.php
+++ partials/footer.php
@@ -1,18 +1,20 @@
-<footer>
-    <div class="contact-info">
-        <p>Email: info@padelclub.com</p>
-        <p>Telefono: 0123456789</p>
-        <p>Indirizzo: Via del Padel, 123</p>
-    </div>
-    <div class="legal-links">
-        <ul>
-            <li><a href="terms.php">Termini e condizioni</a></li>
-            <li><a href="privacy.php">Privacy Policy</a></li>
-            <!-- Aggiungi altri link legali secondo necessità -->
-        </ul>
-    </div>
-    <div class="credits">
-        <p>&copy; 2024 Padel Club</p>
-        <p>Realizzato con passione dal Dream Team</p>
+<footer class="container-fluid">
+    <div class="row pt-3 justify-content-md-center">
+        <div class="col mx-3 col-lg-3">
+            <p>Email: info@padelclub.com</p>
+            <p>Telefono: 0123456789</p>
+            <p>Indirizzo: Via del Padel, 123</p>
+        </div>
+        <div class="col mx-3 col-lg-3">
+            <ul>
+                <li><a href="terms.php">Termini e condizioni</a></li>
+                <li><a href="privacy.php">Privacy Policy</a></li>
+                <!-- Aggiungi altri link legali secondo necessità -->
+            </ul>
+        </div>
+        <div class="col mx-3 col-lg-3">
+            <p>&copy; 2024 Padel Club</p>
+            <p>Realizzato con passione dal Dream Team</p>
+        </div>
     </div>
 </footer>
