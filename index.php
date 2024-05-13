a7f153bb9713cce8f089e05547b7c61fd02fe441
Ricaldone-Gabriel <gabriel.ricaldone@jcmaxwell.it>
2024-05-13 20:13:15 +0200

    Modifica index

--- index.php
+++ index.php
@@ -1,15 +1,41 @@
 <?php
 include('partials/header.php'); ?>
 
+
 <body class="bodyBackground d-flex flex-column min-vh-100">
     <title>Ballin'</title>
     <?php include('partials/navbar.php'); ?>
     <div class="container-fluid">
-        <div class="row section">
+        <!--<div class="row section mb-3" id="section1">
             <div class="imgSection" id="img1"></div>
+        </div>-->
+        <div class="card">
+            <div class="row g-0">
+                <div class="col-md-8 alignVert">
+                    <div class="card-body align-middle">
+                        <h5 class="card-title">Soci competitivi</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <img src="./images/stock/padel-player.webp" class="img-fluid rounded-start" id="img1" alt="...">
+                </div>
+            </div>
         </div>
-        <div class="row section">
-
+        <div class="card">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="./images/stock/padel-player.webp" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8 alignVert">
+                    <div class="card-body align-middle">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <?php include('partials/footer.php'); ?>
