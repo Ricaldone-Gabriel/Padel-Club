b39eb9d1dc8d6a1dbb66c4d5752b8666ca53fc0f
Davide Depetris <davide.depetris@jcmaxwell.it>
2024-05-14 08:49:03 +0200

    booking

--- booking.php
+++ booking.php
@@ -1,6 +1,6 @@
 <?php
-include('./includes/config.php');
-include('./includes/functions.php');
+include ('./includes/config.php');
+include ('./includes/functions.php');
 
 if (isset($_POST['submit_registration'])) {
     $username = $_POST['username'];
@@ -48,10 +48,10 @@ if (isset($_POST['submit_booking'])) {
 }
 ?>
 
-<?php include('./partials/header.php') ?>
+<?php include ('./partials/header.php') ?>
 
 <body class="bodyBackground d-flex flex-column min-vh-100">
-    <?php include('./partials/navbar.php') ?>
+    <?php include ('./partials/navbar.php') ?>
 
     <div class="container">
         <h2>Prenota un Campo</h2>
@@ -86,6 +86,79 @@ if (isset($_POST['submit_booking'])) {
 
             <input type="submit" name="submit_booking" value="Prenota">
         </form>
+        <div class="row g-0">
+            <?php
+            echo $_SESSION['ID'];
+            $query = "SELECT P.CodiceSocio, P.CodiceCampo, P.DataPrenotazione, P.OraPrenotazione
+            FROM Prenotazione P
+            WHERE P.CodiceSocio != '" . $_SESSION['ID'] . "'
+            ORDER BY P.DataPrenotazione DESC, P.OraPrenotazione DESC";
+            $risultato = $connection->query($query);
+
+            if ($risultato->num_rows > 0) {
+                echo "<div class='gradient-custom-1 h-100'>";
+                echo "<div class='mask d-flex align-items-center h-100'>";
+                echo "<div class='container'>";
+                echo "<div class='row justify-content-center'>";
+                echo "<div class='col-12'>";
+                echo "<div class='table-responsive bg-white'>";
+                echo "<h3><caption>Prenotazioni campi</caption></h3>";
+                echo "<table class='table'>";
+                echo "<thead><tr><th scope='col'>ID Utente</th><th scope='col'>Campo</th><th scope='col'>Data</th><th scope='col'>Ora</th><th scope='col'>Soci</th></thead><tbody>";
+
+                while ($row = $risultato->fetch_assoc()) {
+
+                    $query_numero_soci = "SELECT COUNT(S.ID) AS numero_soci
+                      FROM Prenotazione P
+                      JOIN Socio S ON P.CodiceSocio = S.ID
+                      JOIN Campo C ON P.CodiceCampo = C.ID
+                      WHERE P.DataPrenotazione = '" . $row['DataPrenotazione'] . "' 
+                      AND P.OraPrenotazione = '" . $row['OraPrenotazione'] . "' 
+                      AND C.ID = '" . $row['CodiceCampo'] . "'";
+
+                    if ($result_numero_soci = $connection->query($query_numero_soci)) {
+                        $row_numero_soci = $result_numero_soci->fetch_assoc();
+                    } else {
+                        //errore
+                    }
+
+                    if (strtotime($row['DataPrenotazione'] . ' ' . $row['OraPrenotazione']) > time()) {
+                        echo "<tr>";
+                        echo "<td style='color: #00B613;'>" . $row['CodiceSocio'] . "</td>";
+                        echo "<td style='color: #00B613;'>" . $row['CodiceCampo'] . "</td>";
+                        echo "<td style='color: #00B613;'>" . $row['DataPrenotazione'] . "</td>";
+                        echo "<td style='color: #00B613;'>" . $row['OraPrenotazione'] . "</td>";
+                        echo "<td style='color: #A09F9F;'>" . $row_numero_soci['numero_soci'] . "</td>";
+                        echo "</tr>";
+                    } else {
+                        echo "<tr>";
+                        echo "<td style='color: #A09F9F;'>" . $row['CodiceSocio'] . "</td>";
+                        echo "<td style='color: #A09F9F;'>" . $row['CodiceCampo'] . "</td>";
+                        echo "<td style='color: #A09F9F;'>" . $row['DataPrenotazione'] . "</td>";
+                        echo "<td style='color: #A09F9F;'>" . $row['OraPrenotazione'] . "</td>";
+                        echo "<td style='color: #A09F9F;'>" . $row_numero_soci['numero_soci'] . "</td>";
+                        echo "</tr>";
+                    }
+                }
+
+                echo "</tbody></table>";
+
+                // Fine della tabella HTML
+                echo "</tbody>";
+                echo "</table>";
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
+            } else {
+                echo "<div class='m-3 align-items-center'>";
+                echo "Gli altri soci non hanno effettuato prenotazioni";
+                echo "</div>";
+            }
+            ?>
+        </div>
     </div>
 
-    <?php include('./partials/footer.php') ?>
+    <?php include ('./partials/footer.php') ?>
